0bbfce72864f8ec4ffc890d6e991acb0e2713fb0
nunkesser <robin.nunkesser@hshl.de>
2025-09-04 14:23:00 +0200

    9.8.0

--- Italbytz.Adapters.Algorithms.AI/Italbytz.Adapters.Algorithms.AI/Search/EA/Operator/Finish.cs
+++
@@ -1,6 +0,0 @@
-namespace Italbytz.AI.Search.EA.Operator;
-
-public class Finish : GraphOperator
-{
-    public override int MaxChildren { get; } = 0;
-}
--- Italbytz.Adapters.Algorithms.AI/Italbytz.Adapters.Algorithms.AI/Search/EA/Operator/Start.cs
+++
@@ -1,7 +0,0 @@
-namespace Italbytz.AI.Search.EA.Operator;
-
-public class Start : GraphOperator
-{
-    public override int MaxParents { get; } = 0;
-    public override int MaxChildren { get; } = int.MaxValue;
-}
